56b574fd0d7d2f2b3a1410547c740ee0768be970
Fluorky <mrdyrt.inf@gmail.com>
2025-05-25 12:47:28 +0200

    Update script to create symlink with MNIST dataset

--- cycnn/link_mnist.sh
+++ cycnn/link_mnist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/CyCNN/CyCNN-master/cycnn/data
 ln -s /mnt/d/MasterThesis/MasterThesis/dataset/MNIST MNIST_WIN
-echo "✅ Symlink MNIST_WIN created!"
+echo "Symlink MNIST_WIN created correctly!"
