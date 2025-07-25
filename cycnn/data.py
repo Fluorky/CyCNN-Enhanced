26b5fd1dc640834afcf43cba0b001683d41321a1
Fluorky <mrdyrt.inf@gmail.com>
2025-07-25 22:33:18 +0200

    Update data.py - code refactor

--- cycnn/data.py
+++ cycnn/data.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from custom_loader import CustomMNISTDataset, CustomGTSRBDataset
+from custom_loader import CustomIDXDataset, CustomGTSRBDataset
 
 
 def load_mnist_data(data_dir='./data', batch_size=128):
@@ -63,8 +63,8 @@ def load_custom_mnist_data(data_dir='./data', batch_size=128):
     test_images = os.path.join(data_dir, 't10k-images-idx3-ubyte')
     test_labels = os.path.join(data_dir, 't10k-labels-idx1-ubyte')
     
-    train_set = CustomMNISTDataset(images_path=train_images, labels_path=train_labels, transform=train_transform)
-    test_set = CustomMNISTDataset(images_path=test_images, labels_path=test_labels, transform=test_transform)
+    train_set = CustomIDXDataset(images_path=train_images, labels_path=train_labels, transform=train_transform)
+    test_set = CustomIDXDataset(images_path=test_images, labels_path=test_labels, transform=test_transform)
 
     return train_set, test_set
 
